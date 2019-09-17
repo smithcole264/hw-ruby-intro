0a59e68b09afeb066ba08eff6f5f30e2e1d839d4
smithcole264 <55466305+smithcole264@users.noreply.github.com>
2019-09-17 17:40:42 -0500

    inserted function/class definitions

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |i|
+    sum += i
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum_2 = 0
+  sum_2 = arr.sort.last(2).sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  !!arr.uniq.combination(2).detect { |a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  greeting = "Hello, "
+  greeting_name = greeting + name
+  return greeting_name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if /\A(?=[^aeiouAEIOU])(?=[a-z])(?=[A-Z])/i.match(s)
+    return true
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s == "0"
+    return true
+  end
+  if /^[01]*(00)$/.match(s)
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price)
+    raise ArgumentError.new("ISBN must not be empty.") if isbn.nil? || isbn.empty?
+    raise ArgumentError.new("Price must be greater than 0.") if price <= 0
+    
+    @isbn = isbn
+    @price = price
+  end
+  
+  def price_as_string
+    s = '%.2f' % @price
+    sign = "$"
+    format = sign + s
+    return format
+  end
+  
 end
